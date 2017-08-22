ffdc74fd8c0f4852086a1cda6281881460d47aa7
vrde <agranzot@gmail.com>
2017-08-22 15:47:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Introduction
+
+Simple Command Line Interface to generate transactions and send them to BigchainDB.
+
+# Usage
+
+```
+git clone https://github.com/bigchaindb/benchmark.git
+cd benchmark
+pip install -e .
+bigchaindb-benchmark -h
+bigchaindb-benchmark send
+```
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bigchaindb_benchmark',
+    version='0.0.1',
+    description='Command Line Interface to push transactions to BigchainDB',
+    author='BigchainDB devs',
+    packages=find_packages(),
+    install_requires=[
+        'bigchaindb-driver~=0.4.0',
+        'coloredlogs~=7.3.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'bigchaindb-benchmark=bigchaindb_benchmark.commands:main',
+        ],
+    },
+)
